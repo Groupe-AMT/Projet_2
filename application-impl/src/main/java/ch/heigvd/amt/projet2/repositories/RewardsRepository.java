00fcbb80ba3aec6058381a209f9d4d242aabc7d7
jerome <jerome.arn@heig-vd.ch>
2020-11-20 15:06:47 +0100

    rewards

---
+++ application-impl/src/main/java/ch/heigvd/amt/projet2/repositories/RewardsRepository.java
@@ -0,0 +1,8 @@
+package ch.heigvd.amt.projet2.repositories;
+
+
+import ch.heigvd.amt.projet2.entities.RewardsEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RewardsRepository extends CrudRepository<RewardsEntity, Long> {
+}
