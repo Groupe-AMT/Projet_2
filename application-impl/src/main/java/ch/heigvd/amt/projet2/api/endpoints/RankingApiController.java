ba5c6bc54a63307caa8e3e7cf5479d47f5a7bb25
jerome <jerome.arn@heig-vd.ch>
2020-12-05 16:10:14 +0100

    endpoint ranking

---
+++ application-impl/src/main/java/ch/heigvd/amt/projet2/api/endpoints/RankingApiController.java
@@ -0,0 +1,37 @@
+package ch.heigvd.amt.projet2.api.endpoints;
+
+import ch.heigvd.amt.projet2.api.RankingsApi;
+import ch.heigvd.amt.projet2.api.model.Ranking;
+import ch.heigvd.amt.projet2.repositories.BadgeRewardRepository;
+import ch.heigvd.amt.projet2.repositories.EndUserRepository;
+import ch.heigvd.amt.projet2.repositories.PointScaleRewardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RankingApiController implements RankingsApi {
+
+    @Autowired
+    BadgeRewardRepository badgeRewardRepository;
+
+    @Autowired
+    PointScaleRewardRepository pointScaleRewardRepository;
+
+    @Autowired
+    EndUserRepository endUserRepository;
+
+    @Autowired
+    private HttpServletRequest context;
+
+    public ResponseEntity<List<Ranking>> getUserBadges(){
+        List<Ranking> rank = new ArrayList<>();
+        Ranking r = new Ranking();
+
+        return null;
+    }
+
+}
